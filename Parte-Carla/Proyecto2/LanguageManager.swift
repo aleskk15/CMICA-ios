90b4978e0c62b2c8441514799d2fabfe48ed4dc7
Carla Duval <carli@192.168.1.116>
2025-11-21 22:32:18 -0600

    Correcciones, cambio de idioma, encriptación, detalles 2

--- Parte-Carla/Proyecto2/LanguageManager.swift
+++ Parte-Carla/Proyecto2/LanguageManager.swift
@@ -15,12 +15,10 @@ class LanguageManager: ObservableObject {
 
 extension String {
     func traducido(_ idioma: String) -> String {
-        // 1. Busca el archivo de idioma específico (es.lproj o en.lproj)
         guard let path = Bundle.main.path(forResource: idioma, ofType: "lproj"),
               let bundle = Bundle(path: path) else {
             return self // Si falla, regresa el texto original
         }
-        // 2. Fuerza la traducción usando ese archivo
         return NSLocalizedString(self, tableName: nil, bundle: bundle, value: "", comment: "")
     }
 }
